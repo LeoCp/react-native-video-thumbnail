f1d64273f2e2584e3c32212f61d377e6e4fa6949
Caio Rodrigues <caio.rm1@gmail.com>
2023-08-18 14:44:07 -0300

    ios build fixes

--- ios/RNVideoThumbnail.h
+++ ios/RNVideoThumbnail.h
@@ -1,7 +1,13 @@
 #import <AVFoundation/AVFoundation.h>
-@import UIKit;
+#import <UIKit/UIKit.h>
 
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"
+#endif
 
 @interface RNVideoThumbnail : NSObject <RCTBridgeModule>
 
